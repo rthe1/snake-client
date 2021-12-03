a563f5dd6cabe19f62690a4cb5d7d101acfff389
Reshawn <reshawnhamilton97@gmail.com>
2021-12-03 12:14:57 +0000

    how to move

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
+    host: '10.0.2.15',
     port: '50541',
   });
   conn.on('connect', () => {
@@ -13,6 +13,37 @@ const connect = function () {
   conn.on('connect', () => {
     conn.write("Name: 007");
   });
+
+
+  setTimeout(() => {
+    conn.on('connect', () => {
+      conn.write("Move: up");
+    });
+    setTimeout(() => {
+      conn.on('connect', () => {
+        conn.write("Move: left");
+      });
+      setTimeout(() => {
+        conn.on('connect', () => {
+          conn.write("Move: up");
+        });
+        setTimeout(() => {
+          conn.on('connect', () => {
+            conn.write("Move: left");
+          });
+          setTimeout(() => {
+            conn.on('connect', () => {
+              conn.write("Move: up");
+            });
+          }, 50);
+        },100);
+      }, 150);
+    }, 200);
+  }, 250);
+  
+
+  
+
   
   conn.on('data', (data) => {
     console.log(data);
